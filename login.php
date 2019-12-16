0cdb32aa609a1cb0c79548a4b1ac1e0041ea7abb
Dreamnikolaevna <56293764+Dreamnikolaevna@users.noreply.github.com>
2019-12-17 02:19:39 +0300

    change login & register to use functions

--- login.php
+++ login.php
@@ -1,20 +1,20 @@
 <?php
 
-$msg = "";
-
 $login = $_POST['login'];
 $pwd = crypt($_POST['pwd'], "asfsfrwf");
-
 $link = mysqli_connect("localhost", "root", "", "credentials");
-$query = mysqli_query($link, "SELECT pwd FROM users WHERE login='" . mysqli_real_escape_string($link, $login) . "' LIMIT 1");
-$data = mysqli_fetch_assoc($query);
 
-if ($pwd == $data['pwd']) {
-  setcookie("login", $login, 0, "/");
-  $msg = "<script> location.reload(true); </script>";
-} else {
-  $msg = "Неверный логин или пароль";
+function login($link, $login, $pwd) {
+  $query = mysqli_query($link, "SELECT pwd FROM users WHERE login='" . mysqli_real_escape_string($link, $login) . "' LIMIT 1");
+  $data = mysqli_fetch_assoc($query);
+
+  if ($pwd == $data['pwd']) {
+    setcookie("login", $login, 0, "/");
+    return "<script> location.reload(true); </script>"; // перезагрузить с новым куки
+  } else {
+    return "Неверный логин или пароль";
+  }
 }
 
-echo $msg;
+echo login($link, $login, $pwd);
 ?>
--- register.php
+++ register.php
@@ -2,24 +2,22 @@
 
 // admin:admin
 // CREATE TABLE `credentials`.`users` ( `id` INT NOT NULL AUTO_INCREMENT , `login` TEXT NOT NULL , `pwd` TEXT NOT NULL , PRIMARY KEY (`id`)) ENGINE = InnoDB;
-// INSERT INTO `users` (`id`, `login`, `pwd`) VALUES ('0', 'admin', 'asjpY1/u/ny4Q')
-
-$msg = "";
 
 $login = $_POST['login'];
 $pwd = crypt($_POST['pwd'], "asfsfrwf");
-
 $link = mysqli_connect("localhost", "root", "", "credentials");
-$query = mysqli_query($link, "SELECT id FROM users WHERE login='" . mysqli_real_escape_string($link, $login) . "'");
-if (mysqli_num_rows($query) > 0) {
-  $msg = "Пользователь " . $login . " уже зарегестрирован";
-} else {
+
+function register($link, $login, $pwd) {
+  $query = mysqli_query($link, "SELECT id FROM users WHERE login='" . mysqli_real_escape_string($link, $login) . "'");
+  if (mysqli_num_rows($query) > 0)
+    return "Пользователь " . $login . " уже зарегестрирован";
   if (!mysqli_query($link, "INSERT INTO users SET login='" . $login . "', pwd='" . $pwd . "'")) {
-    $msg = "Создать пользователя " . $login . " не удалось";
+      return "Создать пользователя " . $login . " не удалось";
   } else {
     setcookie("login", $login, 0, "/");
-    $msg = "<script> location.reload(true); </script>";
+    return "<script> location.reload(true); </script>"; // перезагрузить страницу с новым куки
   }
 }
-echo $msg
+
+echo register($link, $login, $pwd);
 ?>
